02ddf5b9cb3e100cae97a5d697dde96830ec237a
Harjot Grewal <grewal@Harjots-MacBook-Pro.local>
2026-05-08 19:55:51 -0400

    Updated multi-AZ VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,19 +9,6 @@ resource "aws_vpc" "this" {
   )
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.project}-${var.environment}-public-subnet"
-    }
-  )
-}
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.this.id
 
@@ -33,6 +20,7 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
+# Public Subnet - AZ A
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.1.0/24"
@@ -47,6 +35,7 @@ resource "aws_subnet" "public_a" {
   )
 }
 
+# Public Subnet - AZ B
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.2.0/24"
@@ -61,11 +50,30 @@ resource "aws_subnet" "public_b" {
   )
 }
 
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project}-${var.environment}-public-rt"
+    }
+  )
+}
+
+# Route Table Association - AZ A
 resource "aws_route_table_association" "public_assoc_a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public.id
 }
 
+# Route Table Association - AZ B
 resource "aws_route_table_association" "public_assoc_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
